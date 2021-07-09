d2f0b148d29b7fcc50340699ae2252298393a39a
fabian-guevara <fabiangviveros@gmail.com>
2021-07-08 20:52:33 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+        name: "Lionel Messi",
+        club: "FC Barcelona",
+        age: 33,
+        id: 1
+    },
+    {
+        name: "Antoine Griezmann",
+        club: "FC Barcelona",
+        age: 27,
+        id: 4
+
+    },
+    {
+        name: "Antony Silva",
+        club: "Puebla",
+        age:  37,
+        id: 3
+
+    },
+    {
+        name: "Cuauhtemoc Blanco",
+        club: "Retirado",
+        age: 40,
+        id: 2
+
+    }
+];
+
