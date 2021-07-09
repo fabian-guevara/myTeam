6bd3beb04f4407b26fd8ab1bcb4e8466966952af
fabian-guevara <fabiangviveros@gmail.com>
2021-07-08 21:05:16 -0500

    fixed typos

--- src/PlayerCard.js
+++ src/PlayerCard.js
@@ -1,34 +1,31 @@
 import React, { useState, } from "react";
 import { data, } from "./data";
 import "./App.css";
-const PlayerCard = () => {
-        
-    const [team , setTeam] = useState(data);
-    const removePlayer = (remove) => {
-        const newTeam = team.filter(player => player.id !== remove)
-        setTeam([...newTeam])
-    }
 
+
+const PlayerCard = () => { 
+    const [team , setTeam] = useState(data);
+    const removePlayer = (id) => { setTeam(team.filter(player => player.id !== id)) };
   return (
     <>
-        <div key={100} className="wrapper">
+        <div className="wrapper">
             <h1 className="title">My team</h1>
-                {
-            team.map((player) => {
-             const { name, club, age, id, } = player;   
-            return <div key={id} className="card">
-                <h3>{ name }</h3>
-                <p> { club }</p>
-                <p> { age }</p>
-                <button className="discard-btn" onClick={() => removePlayer(id)}>Discard</button>
-                </div>    
-            })
+            {
+                team.map((player) => {
+                    const { name, club, age, id, } = player;   
+                    return <div key={id} className="card">
+                    <h3>{ name }</h3>
+                    <p> { club }</p>
+                    <p> { age }</p>
+                    <button className="discard-btn" onClick={() => removePlayer(id)}>Discard</button>
+                    </div>    
+                })
             }
         <button className="discardAll-btn" onClick={() => setTeam([])}>Clear Team</button>
         </div>
     </>
   );
-}
+};
 
 
 export default PlayerCard;
